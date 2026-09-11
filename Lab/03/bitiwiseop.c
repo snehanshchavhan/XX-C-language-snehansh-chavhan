83c1d2cc171c1d0b1c4f89915e428ac620bda855
snehanshchavhan <snehanshcode@gmail.com>
2026-09-11 22:09:05 +0530

    Add files via upload

---
+++ Lab/03/bitiwiseop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a=25, b=5;
+    printf("a&b=%d\n", a&b);
+    printf("a|b=%d\n", a|b);
+    printf("a^b=%d\n", a^b);
+    printf("~b=%d\n", ~b);
+    printf("a>>b=%d\n", a>>b);
+    printf("a<<b=%d\n", a<<b);
+
+    return 0;
+}
