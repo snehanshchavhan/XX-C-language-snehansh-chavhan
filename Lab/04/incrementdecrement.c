ea66d61a6ab8a6e65aa159b67078b67387b62e0b
snehanshchavhan <snehanshcode@gmail.com>
2026-09-11 22:31:20 +0530

    Add files via upload

--- Lab/04/incrementdecrement.c
+++ Lab/04/incrementdecrement.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter the number a: ");
+    scanf("%d", &a);
+    printf("a= %d\n", a);
+    printf("value of a after increment= %d\n", ++a);
+    printf("valuae of a after decrement= %d\n", --a);
 
+    return 0;
+}
